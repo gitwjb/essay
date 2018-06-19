9ab8199475181197b11d5577e9a5240ff6b82f3c
wjb <emailwjb@163.com>
2018-06-19 12:52:04 +0800

    ff

---
+++ test/src/com/wjb/test/Test.java
@@ -0,0 +1,11 @@
+package com.wjb.test;
+
+public class Test {
+	public static void main(String[] args) {
+		String str = ""+/**~{*/""
+			+ "eclipse 很好用"
+			+ "\r\n支持 多行字符串编辑"
+		+ "\r\n"/**}*/;
+		System.out.println(str);
+	}
+}
