3ae961d055e61633852dfa1b4e0c9c7c7519e5d9
wjb <emailwjb@163.com>
2018-11-21 14:01:47 +0800

    list memory size

---
+++ test/src/com/wjb/collection/TestList.java
@@ -0,0 +1,18 @@
+package com.wjb.collection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TestList {
+	public static void main(String[] args) {
+		List<Object> lst = new ArrayList<>();
+		Map<String, Object> hm = new HashMap<>();
+		hm.put("name", "张三");
+		lst.add(hm);
+		
+		System.out.println(Arrays.toString(lst.toArray()).getBytes().length);
+	}
+}
---
+++ test/src/com/wjb/collection/TestMap.java
@@ -0,0 +1,13 @@
+package com.wjb.collection;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestMap {
+	public static void main(String[] args) {
+		Map<String, Object> hm = new HashMap<>();
+		
+		System.out.println();
+	}
+}
