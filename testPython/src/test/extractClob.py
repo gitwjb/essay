0722c1b1eb8a2212fe75f39186ebcd39b97a504b
wjb <emailwjb@163.com>
2018-12-28 15:12:01 +0800

    c

--- testPython/src/test/extractClob.py
+++ testPython/src/test/extractClob.py
@@ -8,13 +8,15 @@ import time
 import cx_Oracle
 import os  
 os.environ['NLS_LANG'] = 'SIMPLIFIED CHINESE_CHINA.UTF8' 
-#cx_Oracle.connect("bidw","acca_1234","10.1.19.149:1521/bidwcz")
+
 print time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
-conn = cx_Oracle.connect('bidw/acca_1234@10.1.19.149:1521/bidwcz')
+conn = cx_Oracle.connect('smartrix_dev/smartrix_1234@10.1.15.55:1523/SMXT')
 cursor = conn.cursor()
-cursor.execute("SELECT * FROM dwp_psalcpnm_201811271")
+cursor.execute("SELECT length(text),text FROM test")
 rows = cursor.fetchone()
-print rows[0]
+print rows[0],rows[1]
+print rows
+print (rows)
 print time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
 cursor.close()
 conn.close()
